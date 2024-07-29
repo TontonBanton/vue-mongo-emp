5e2ed8267a1e0d9cc3de6ecdb8eeec6e964db23b
TontonBanton <pancla13ulas@gmail.com>
2024-07-29 09:15:42 +0800

    Router routes added, cors

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
+const express = require('express');
+const server = express();
 
-// create out express app
-const app = express()
-app.use(bodyParser.json())
+const cors = require('cors');
+server.use(cors());
+
+const routes = require('./routes/routes');
+server.use(routes);
+
 
 // database stuff
+const mongoose = require("mongoose")
 mongoose.connect("mongodb://127.0.0.1:27017/emp_crud")
-.then(() => console.log("Connected"))
+.then(() => console.log("MongoDB Connected"))
 .catch((error) => console.log(error));
 
+
 // start server
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log("Listening at port 3000")
 })
